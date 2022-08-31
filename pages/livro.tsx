320a4a71688fe8340f459b039a57a1825d26b541
Ruy Freire <ruy.slv@gmail.com>
2022-08-30 22:50:52 -0300

    livro: tela de livros

--- pages/livro.tsx
+++ pages/livro.tsx
@@ -1,12 +1,53 @@
+import Image from 'next/image'
 import React from 'react'
 
+import { Box, Button, Grid, Tooltip } from 'ui-app'
+
+import { Section } from 'components'
 import Layout from 'templates/Layout'
 import routes from 'utils/routes'
 
 const Livro = () => {
   return (
     <Layout title={routes.livro.name} header={routes.livro.name}>
-      <p>Livro</p>
+      <Section>
+        <Box display="flex" justifyContent="center" bgColor="#fff">
+          <Image
+            src="/books.jpg"
+            alt="Ilustração de estante com livros"
+            height={200}
+            width={300}
+          />
+        </Box>
+      </Section>
+
+      <Grid container flexDirection="column">
+        <Grid item>
+          <Button variant="ghost" fullWidth>
+            Buscar por nome ou autor
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Button variant="ghost" fullWidth>
+            Buscar todos
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Button variant="ghost" fullWidth>
+            Cadastrar
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Tooltip text="Em breve">
+            <Button variant="ghost" fullWidth disabled>
+              Emprestar
+            </Button>
+          </Tooltip>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
