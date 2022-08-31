56e4bb0cc092828ef6ea3434c25d9d252128e97a
Ruy Freire <ruy.slv@gmail.com>
2022-08-30 21:21:21 -0300

    config next

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'styled-components': path.resolve(
+        __dirname,
+        'node_modules/styled-components'
+      ),
+      react: path.resolve(__dirname, 'node_modules/react'),
+    }
+
+    return config
   },
 }
 
