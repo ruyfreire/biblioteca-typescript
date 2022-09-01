98504b8fe2c3029b6c25b8469c40a5ea3fb3a36f
Ruy Freire <ruy.slv@gmail.com>
2022-09-01 00:26:58 -0300

    rotas: melhoria de rotas

--- utils/routes.ts
+++ utils/routes.ts
@@ -2,14 +2,22 @@ const routes = {
   home: {
     name: 'Início',
     path: '/',
+    showMenu: true,
   },
   livros: {
     name: 'Livros',
     path: '/livros',
+    showMenu: true,
   },
   autores: {
     name: 'Autores',
     path: '/autores',
+    showMenu: true,
+  },
+  autoresBuscar: {
+    name: 'Buscar autores',
+    path: '/autores/buscar',
+    showMenu: false,
   },
 }
 
