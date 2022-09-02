2823b28e620464fef264282e3b8651e407c5b7b6
Ruy Freire <ruy.slv@gmail.com>
2022-09-01 23:18:43 -0300

    layout: melhoria de estilo

--- templates/Layout/styles.tsx
+++ templates/Layout/styles.tsx
@@ -8,72 +8,74 @@ export const Container = styled.div<ContainerProps>`
   ${({ theme, hasSection }) => css`
     --footer-height: 72px;
     --head-height: 64px;
+    background-color: ${theme.colors.background.section};
     display: grid;
     grid-template-columns: 1fr;
     min-height: 100vh;
-    background-color: ${theme.colors.background.section};
 
     ${hasSection
       ? css`
-          grid-template-rows: 64px min-content 1fr var(--footer-height);
           grid-template-areas:
             'head'
             'section'
             'main'
             'footer';
+          grid-template-rows: 64px min-content 1fr var(--footer-height);
         `
       : css`
-          grid-template-rows: 64px 1fr;
           grid-template-areas:
             'head'
             'main'
             'footer';
+          grid-template-rows: 64px 1fr var(--footer-height);
         `}
 
     header {
-      grid-area: head;
-      z-index: 10;
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      background-color: ${theme.colors.background.primary};
-      padding: 8px 16px;
-      height: var(--head-height);
-      display: flex;
       align-items: center;
+      background-color: ${theme.colors.background.primary};
+      display: flex;
+      grid-area: head;
+      height: var(--head-height);
+      left: 0;
+      padding: 8px 16px;
+      position: fixed;
+      right: 0;
+      top: 0;
+      z-index: 10;
     }
 
     main {
       grid-area: main;
+      display: flex;
+      flex-direction: column;
+      margin: 0 auto;
+      max-width: 600px;
       padding: 16px;
       width: 100%;
-      max-width: 1200px;
-      margin: 0 auto;
     }
 
     nav {
-      grid-area: footer;
-      z-index: 10;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      height: var(--footer-height);
       background-color: ${theme.colors.background.primary};
+      bottom: 0;
+      grid-area: footer;
+      height: var(--footer-height);
+      left: 0;
       padding: 8px 16px;
+      position: fixed;
+      right: 0;
+      z-index: 10;
     }
   `}
 `
 
 export const TabLink = styled.a`
-  display: flex;
   align-items: center;
+  color: inherit;
+  display: flex;
+  font: inherit;
+  height: 100%;
   justify-content: center;
   width: 100%;
-  height: 100%;
-  font: inherit;
-  color: inherit;
 
   &,
   :active,
@@ -83,11 +85,11 @@ export const TabLink = styled.a`
 `
 
 export const LogoHeader = styled.a`
-  display: flex;
   align-items: center;
-  justify-content: center;
-  text-decoration: none;
   color: inherit;
-  padding: 8px 0;
+  display: flex;
   gap: 8px;
+  justify-content: center;
+  padding: 8px 0;
+  text-decoration: none;
 `
