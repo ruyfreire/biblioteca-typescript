9e24815d453cfc3fc7276e7d8c307a99cf1cec5d
Ruy Freire <ruy.slv@gmail.com>
2022-09-07 23:04:26 -0300

    app Layout das paginas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 
 import { theme, GlobalStyle } from 'ui-app'
 
+import { Nav } from 'components'
+import { store } from 'redux/store'
 import { ThemeProvider } from 'styled-components'
+import Layout from 'templates/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps} />
+
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+
+          <Nav />
+        </Layout>
+      </Provider>
     </ThemeProvider>
   )
 }
